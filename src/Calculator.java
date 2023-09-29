6866e94b42868f2d257bdc07ac850513949b40c2
HwuShyam2005 <shyam164825@gmail.com>
2023-09-29 23:46:26 +0400

    FINAL CODE WITH COMMENTS

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,24 +11,29 @@ public class Calculator {
 	 *  Methods for Part 3
 	 */
 	public static int calculate(String [] cmds){
-		Stack pstack = new Stack(100);
+		//CREATION OF A NEW STACK 'NAME' TO STORE RESULTS
+		Stack name = new Stack(100);
 		int result = 0;
+		//REVERSING THE INPUTS
 		Reverse.reverse(cmds);
 		
+		//PUSHING IT ONTO STACK IF THE ELEMENT IS A NUMBER
 		for (int i =0; i < cmds.length; i++) {
 			if(isNumber(cmds[i])) {
-				pstack.push(convert(cmds[i]));
+				name.push(convert(cmds[i]));
+			//IF ELEMENT IS AN OPERATOR THEN IT APPLIES THE OPERATION ON THE TOP TWO ELEMENTS OF THE STACK
 			}else {
-				result = applyOp(pstack.pop().toString(), cmds[i], pstack.pop().toString());
-			    pstack.push(result);
+				result = applyOp(name.pop().toString(), cmds[i], name.pop().toString());
+			    name.push(result);
 			}
 			
 		}
 		
 		
-		return result; // dummy value
+		return result; // VALUE AT THE TOP OF THE STACK
 	}	
 
+	//CONVERTING AN STRING TO INTEGER ALONG WITH HANDLING THE ERRORS
 	public static int convert(String s) throws NumberFormatException{
 		int myNum = 0;
 		
@@ -36,22 +41,24 @@ public class Calculator {
 			myNum = Integer.parseInt(s);
 		}
 		
-		return myNum; // dummy value
+		return myNum; // DUMMY VALUE
 	}
 
 	public static boolean isNumber(String s){
 		boolean myBool = false;
 		try {
+		//IF THE CONDITION TO PARSE THE STRING IS AN INTEGER
 			if ( Integer.parseInt(s)* 0 == 0) 
 				myBool = true;
 		} catch (NumberFormatException e) {
+		//INCASE IF IT FAILS, THEN IT ISNT A VALID NUMBER	
 			myBool = false;
 		}
 		
-		return myBool; // dummy value
+		return myBool; // DUMMY VALUE
 	}
 
-	// apply the operator after converting the numbers
+	//APPLY THE OPERATOR AFTER CONVERTING THE NUMBERS
 	public static int applyOp(String fst,String op,String snd){
 		
 		int j1 = convert(fst);
@@ -69,11 +76,11 @@ public class Calculator {
 			return j1 / j2;
 		}
 		
-		return 0; // dummy value
+		return 0; // DUMMY VALUE
 	}
 
-	// main operation to calculate using Polish notation directly
+	//MAIN OPERATION TO CALCULATE USING POLISH NOTATION DIRECTLY
     public static int calculatePolish(String [] cmds){
-    	return -1; // dummy value
+    	return -1; // DUMMY VALUE
     }
 }
--- src/Reverse.java
+++ src/Reverse.java
@@ -7,12 +7,17 @@ public class Reverse {
 	//
 	// O(?)
 	public static void reverse(String[] arr){ 
-		Stack st= new Stack(arr.length);
+		//CREATION OF NEW STACK 'STK' WITH LENGTH SAME AS THE 'ARR' ARRAY.
+		Stack stk= new Stack(arr.length);
+		
+		// PUSHING OF EACH ELEMENT FROM THE 'ARR' ONTO THE STACK 'STK'
 		for(int i=0; i<arr.length; i++) {
-			st.push(arr[i]);
+			stk.push(arr[i]);
 		}
+		
+		//POP ELEMENTS FROM STACK 'STK' AND ASSIGNING THEM AGAIN TO 'ARR' ARRAY
 		for(int i=0; i< arr.length; i++) {
-			arr[i]= (String) st.pop();
+			arr[i]= (String) stk.pop();
 		}
 
 	}
